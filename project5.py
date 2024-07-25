325fee33b6e30c4f88428015162496abbf595774
sangsoong <baejw1031@gmail.com>
2024-07-26 00:44:38 +0900

    1

--- project5.py
+++
@@ -1,9 +0,0 @@
-list1 = [1, 2, 3, 4, 5, 6, 7, 8]
-for i in list1:
-    print(i)
-
-for idx, item in enumerate(list1):
-    print(idx, item)
-
-for dan, gop in enumerate(list1):
-    print(f"{dan+1} x {gop+1} = {(dan+1)*(gop+1)}")
