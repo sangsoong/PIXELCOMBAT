ce0e6d8cc72b86a752d0e159aeb3e25129414ba3
sangsoong <baejw1031@gmail.com>
2024-08-09 01:50:35 +0900

    update

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import pygame
 import sys
 import random
 
+from paths import *
+
 # ======================================== class ========================================
 
 class Button:
@@ -48,6 +50,7 @@ class Floor:
         self.rect = self.image.get_rect()
         self.rect.x = 0
         self.rect.y = 0
+        self.rect.center = (self.rect.x, self.rect.y)
 
     def re_pos(self):
         if self.rect.x <= WIDTH * (-0.5):
@@ -63,18 +66,21 @@ class Floor:
         L_Floor.blit(self.image, self.rect)
 
 class Player:
-    def __init__(self, image, x, y):
-        self.image = pygame.image.load(image).convert_alpha()
-        self.image = pygame.transform.scale(self.image, (50, 50))
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
-        self.rect.center = (x, y)
+    def __init__(self, x, y):
+        self.image_seq = 0
+        self.image_mode = "idle"
+        self.image = pygame.image.load(START_PLAYER_IDLE1).convert_alpha()
+        self.image = pygame.transform.scale(self.image, (100, 100))
+        self.rect = self.image.get_rect(center=(WIDTH/2, HEIGHT/2))
         self.dir_up = 0
         self.dir_left = 0
         self.dir_down = 0
         self.dir_right = 0
         self.speed = 3
+
+    def adjust_image(self, image, last_center):
+        self.image = image
+        self.rect = self.image.get_rect(center=last_center)
 
     def move_to(self, key, push):
         if key == pygame.K_w:
@@ -87,29 +93,53 @@ class Player:
             self.dir_right = push * 1 * self.speed
 
     def move(self):
+        cof = 1
         for objects in start_nonplayer_moving_objects:
             for object in objects:
-                object.rect.y -= self.dir_up
-                object.rect.x -= self.dir_left
-                object.rect.y -= self.dir_down
-                object.rect.x -= self.dir_right
-
-        '''
-        for objects in self.collides:
+                if objects in start_collidable_objects and pygame.sprite.collide_mask(self, object):
+                    cof = 0.7
+                    break
+        for objects in start_nonplayer_moving_objects:
             for object in objects:
-                if pygame.sprite.collide_mask(self, object):
-                    self.rect.y -= self.dir_up
-                    self.rect.x -= self.dir_left
-                    self.rect.y -= self.dir_down
-                    self.rect.x -= self.dir_right
-        '''
+                object.rect.y -= self.dir_up * cof
+                object.rect.x -= self.dir_left * cof
+                object.rect.y -= self.dir_down * cof
+                object.rect.x -= self.dir_right * cof
 
     def draw(self):
+        if self.dir_up == 0 and self.dir_left == 0 and self.dir_down == 0 and self.dir_right == 0:
+            start_player.image_mode = "idle"
+        elif self.dir_up == -1*self.speed or self.dir_left == -1*self.speed or self.dir_right == -1*self.speed or self.dir_left == -1*self.speed:
+            start_player.image_mode = "walk"
+        self.image_seq += 1
+        path = ""
+        if self.image_mode == "idle":
+            if self.image_seq >= len(START_PLAYER_IDLE)*10:
+                self.image_seq = 0
+            path = START_PLAYER_IDLE[self.image_seq // 10]
+        elif self.image_mode == "walk":
+            if self.image_seq >= len(START_PLAYER_WALK)*10:
+                self.image_seq = 0
+            path = START_PLAYER_WALK[self.image_seq // 10]
+        image = pygame.image.load(path).convert_alpha()
+        image = pygame.transform.scale(image, (100, 100))
+        self.adjust_image(image, self.rect.center)
+        
+        # 마우스 보기
+        rel_x = pygame.mouse.get_pos()[0] - self.rect.centerx
+        rel_y = self.rect.centery - pygame.mouse.get_pos()[1]
+        angle = (180/math.pi) * math.atan2(rel_y, rel_x) + 90
+        print(angle)
+        self.image = pygame.transform.rotate(self.image, angle)
+
         L_Player.blit(self.image, self.rect)
 
 class Enemy:
-    summon_delay = 1.5  # must be greater than 1
-    summon_onoff = False
+    names = ["zombie1"]
+    summon_delay = {"zombie1": 1.5,  # must be greater than 1
+                    "none": 0}
+    summon_onoff = {"zombie1": False,
+                    "none": False}
 
     @classmethod
     def summon(cls, current_sec):
@@ -122,25 +152,28 @@ class Enemy:
             ran_x = -50 if random.randint(1, 2) == 1 else WIDTH + 50
             ran_y = random.randint(0, HEIGHT)
 
-        if cls.summon_delay - (current_sec % cls.summon_delay) - 0.1 < 0:
-            if cls.summon_onoff:
-                start_enemies.append(Enemy(START_ENEMY, ran_x, ran_y))
-                cls.summon_onoff = False
-        else:
-            cls.summon_onoff = True
+        for name in cls.names:
+            if cls.summon_delay[name] - (current_sec % cls.summon_delay[name]) - 0.1 < 0:
+                if cls.summon_onoff[name]:
+                    start_enemies.append(Enemy(ran_x, ran_y, name))
+                    cls.summon_onoff[name] = False
+            else:
+                cls.summon_onoff[name] = True
 
-    def __init__(self, image, x, y):
-        self.image = pygame.image.load(image).convert_alpha()
-        self.image = pygame.transform.scale(self.image, (50, 50))
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
-        self.rect.center = (x, y)
-        self.speed = 1
+    def __init__(self, x, y, name):
+        self.image_seq = 0
+        self.image_mode = "run"
+        self.image = pygame.image.load(EFFECT_BLACK).convert_alpha()
+        self.rect = self.image.get_rect(center=(x, y))
+        self.name = name
+        if name == "zombie1":
+            self.speed = 1
+        elif name == "none":
+            self.speed = 2
 
     def move(self, target):
-        off_x = target.rect.x - self.rect.x
-        off_y = target.rect.y - self.rect.y
+        off_x = target.rect.centerx - self.rect.centerx
+        off_y = target.rect.centery - self.rect.centery
         dist = math.sqrt(off_x*off_x + off_y*off_y)
         if dist != 0:
             cos = off_x / dist
@@ -148,13 +181,33 @@ class Enemy:
             speed_x = cos * self.speed
             speed_y = sin * self.speed
 
-            self.rect.x += speed_x
-            self.rect.y += speed_y
+            self.rect.centerx += speed_x
+            self.rect.centery += speed_y
             if pygame.sprite.collide_mask(self, target):
-                self.rect.x -= speed_x
-                self.rect.y -= speed_y
+                self.rect.centerx -= speed_x
+                self.rect.centery -= speed_y
+    
+    def adjust_image(self, image, last_center):
+        self.image = image
+        self.rect = self.image.get_rect(center=last_center)
 
     def draw(self):
+        self.image_seq += 1
+        path = ""
+        if self.name == "zombie1":
+            if self.image_mode == "idle":
+                if self.image_seq >= len(START_ENEMY1_IDLE)*10:
+                    self.image_seq = 0
+                path = START_ENEMY1_IDLE[self.image_seq // 10]
+            elif self.image_mode == "run":
+                if self.image_seq >= len(START_ENEMY1_WALK)*10:
+                    self.image_seq = 0
+                path = START_ENEMY1_WALK[self.image_seq // 10]
+        
+        image = pygame.image.load(path).convert_alpha()
+        image = pygame.transform.scale(image, (100, 100))
+        self.adjust_image(image, self.rect.center)
+
         L_Enemy.blit(self.image, self.rect)
 
 # ======================================== setting ========================================
@@ -175,32 +228,22 @@ pygame.display.set_caption("hackAslash")
 SCREEN.fill((255, 255, 255))
 pygame.display.update()
 
-# ========== path ==========
-EFFECT_BLACK = "sprites/effect_black.png"
-MENU_BACKGROUND = "sprites/menu_background.png"
-MENU_START = "sprites/menu_start.png"
-MENU_START_HOVER = "sprites/menu_start_hover.png"
-MENU_EXIT = "sprites/menu_exit.png"
-MENU_EXIT_HOVER = "sprites/menu_exit_hover.png"
-START_GROUND1 = "sprites\start_ground\grass_1.png"
-START_PLAYER = "sprites/start_player.png"
-START_ENEMY = "sprites/start_enemy.png"
-
 # ========== layer ==========
 layers = {}
-L_EFFECT = pygame.Surface((WIDTH,HEIGHT), pygame.SRCALPHA)
+L_Effect = pygame.Surface((WIDTH,HEIGHT), pygame.SRCALPHA)
 L_Menu = pygame.Surface((WIDTH, HEIGHT), pygame.SRCALPHA)
 L_Intro = pygame.Surface((WIDTH, HEIGHT), pygame.SRCALPHA)
 L_Floor = pygame.Surface((WIDTH,HEIGHT), pygame.SRCALPHA)
 L_Enemy = pygame.Surface((WIDTH,HEIGHT), pygame.SRCALPHA)
 L_Player = pygame.Surface((WIDTH,HEIGHT), pygame.SRCALPHA)
 
-layers[0] = [L_Menu]
-layers[1] = [L_Floor, L_Enemy, L_Player]
+layers[0] = [L_Menu, L_Effect]
+layers[1] = [L_Floor, L_Enemy, L_Player, L_Effect]
 
 # ========== sprite ==========
 # effect
 effect_black = pygame.image.load(EFFECT_BLACK).convert_alpha()
+effect_fog = pygame.image.load(EFFECT_FOG).convert_alpha()
 
 # menu
 menu_background = pygame.image.load(MENU_BACKGROUND).convert_alpha()
@@ -218,21 +261,17 @@ menu_exit = Button("menu_exit", MENU_EXIT, WIDTH*1/10, HEIGHT*9/10, 100, 50)
 start_floors = []
 for i in range(0, 3):
     for j in range(0, 3):
-        floor = Floor(START_GROUND1)
+        floor = Floor(START_GROUND2)
         floor.rect.x = (WIDTH/2)*j
         floor.rect.y = (HEIGHT/2)*i
         start_floors.append(floor)
-start_player = Player(START_PLAYER, WIDTH / 2, HEIGHT / 2)
+start_player = Player(WIDTH / 2, HEIGHT / 2)
 start_enemies = []
 
-start_movable_objects = []
-start_movable_objects.append([start_player])
-start_movable_objects.append(start_enemies)
-start_movable_objects.append(start_floors)
+start_nonplayer_moving_objects = [start_floors, start_enemies]
+start_collidable_objects = [start_enemies]
 
-start_nonplayer_moving_objects = []
-start_nonplayer_moving_objects.append(start_enemies)
-start_nonplayer_moving_objects.append(start_floors)
+start_fog = Effect(EFFECT_FOG, 0, 0, WIDTH, HEIGHT)
 
 # end
 
@@ -261,17 +300,18 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if menu_start.rect.collidepoint(event.pos):
-                    state = 1
-                if menu_exit.rect.collidepoint(event.pos):
-                    run = False
-            if event.type == pygame.KEYDOWN:
-                start_player.move_to(event.key, 1)
-            if event.type == pygame.KEYUP:
-                start_player.move_to(event.key, 0)
+            if state == 0:
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if menu_start.rect.collidepoint(event.pos):
+                        state = 1
+                    if menu_exit.rect.collidepoint(event.pos):
+                        run = False
+            if state == 1:
+                if event.type == pygame.KEYDOWN:
+                    start_player.move_to(event.key, 1)
+                if event.type == pygame.KEYUP:
+                    start_player.move_to(event.key, 0)
 
-        # menu : 0
         if state == 0:
             L_Menu.blit(menu_background, (0, 0))
             menu_start.draw(L_Menu)
@@ -289,37 +329,36 @@ def main():
 
             # effect
             for menu_effect in menu_effects:
-                print(menu_effect.rect.x)
                 if menu_effect.rect.x < -1000:
                     menu_effect.rect.x = random.randint(WIDTH, WIDTH+1000)
                     menu_effect.rect.y = random.randint(int(HEIGHT*(1/20)), int(HEIGHT*(19/20)))
                 else:
                     menu_effect.rect.x -= 15
-                    menu_effect.draw(L_Menu)
+                    menu_effect.draw(L_Effect)
 
-        # start : 1
         if state == 1:
             # floor
             for floor in start_floors:
                 floor.re_pos()
+                floor.draw()
 
             # enemy
             Enemy.summon(current_sec)
             for enemy in start_enemies:
                 enemy.move(start_player)
+                enemy.draw()
 
             # player
             start_player.move()
+            start_player.draw()
 
-        # end : 2
+            # effect
+            start_fog.draw(L_Effect)
+
         if state == 2:
             pass
 
         # ========== update ==========
-        # update object
-        for objects in start_movable_objects:
-            for object in objects:
-                object.draw()
         # update layer
         for key, value in layers.items():
             if key == state:
---
+++ paths.py
@@ -0,0 +1,54 @@
+import pygame
+
+EFFECT_BLACK = "sprites\effect_effects\effect_black.png"
+EFFECT_FOG = "sprites\effect_effects\effect_fog.png"
+
+MENU_BACKGROUND = "sprites\menu_gui\menu_background.png"
+MENU_START = "sprites\menu_gui\menu_start.png"
+MENU_START_HOVER = "sprites\menu_gui\menu_start_hover.png"
+MENU_EXIT = "sprites\menu_gui\menu_exit.png"
+MENU_EXIT_HOVER = "sprites\menu_gui\menu_exit_hover.png"
+
+START_GROUND1 = "sprites\start_ground\grass1.png"
+START_GROUND2 = "sprites\start_ground\cobblestone1.png"
+
+START_PLAYER_IDLE1 = "sprites\\start_player\\idle\\tile000.png"
+START_PLAYER_IDLE = [START_PLAYER_IDLE1]
+START_PLAYER_WALK1 = "sprites\\start_player\\walk\\tile000.png"
+START_PLAYER_WALK2 = "sprites\\start_player\\walk\\tile001.png"
+START_PLAYER_WALK3 = "sprites\\start_player\\walk\\tile002.png"
+START_PLAYER_WALK4 = "sprites\\start_player\\walk\\tile003.png"
+START_PLAYER_WALK5 = "sprites\\start_player\\walk\\tile004.png"
+START_PLAYER_WALK6 = "sprites\\start_player\\walk\\tile005.png"
+START_PLAYER_WALK7 = "sprites\\start_player\\walk\\tile006.png"
+START_PLAYER_WALK8 = "sprites\\start_player\\walk\\tile007.png"
+START_PLAYER_WALK9 = "sprites\\start_player\\walk\\tile008.png"
+START_PLAYER_WALK = [START_PLAYER_WALK1,
+                    START_PLAYER_WALK2,
+                    START_PLAYER_WALK3,
+                    START_PLAYER_WALK4,
+                    START_PLAYER_WALK5,
+                    START_PLAYER_WALK6,
+                    START_PLAYER_WALK7,
+                    START_PLAYER_WALK8,
+                    START_PLAYER_WALK9,]
+START_ENEMY1_IDLE1 = "sprites\\start_enemy\\zombie1\\idle\\tile000.png"
+START_ENEMY1_IDLE = [START_ENEMY1_IDLE1]
+START_ENEMY1_WALK1 = "sprites\\start_enemy\\zombie1\\walk\\tile000.png"
+START_ENEMY1_WALK2 = "sprites\\start_enemy\\zombie1\\walk\\tile001.png"
+START_ENEMY1_WALK3 = "sprites\\start_enemy\\zombie1\\walk\\tile002.png"
+START_ENEMY1_WALK4 = "sprites\\start_enemy\\zombie1\\walk\\tile003.png"
+START_ENEMY1_WALK5 = "sprites\\start_enemy\\zombie1\\walk\\tile004.png"
+START_ENEMY1_WALK6 = "sprites\\start_enemy\\zombie1\\walk\\tile005.png"
+START_ENEMY1_WALK7 = "sprites\\start_enemy\\zombie1\\walk\\tile006.png"
+START_ENEMY1_WALK8 = "sprites\\start_enemy\\zombie1\\walk\\tile007.png"
+START_ENEMY1_WALK9 = "sprites\\start_enemy\\zombie1\\walk\\tile008.png"
+START_ENEMY1_WALK = [START_ENEMY1_WALK1,
+                    START_ENEMY1_WALK2,
+                    START_ENEMY1_WALK3,
+                    START_ENEMY1_WALK4,
+                    START_ENEMY1_WALK5,
+                    START_ENEMY1_WALK6,
+                    START_ENEMY1_WALK7,
+                    START_ENEMY1_WALK8,
+                    START_ENEMY1_WALK9,]
